822def94336a1c45a207204754c0af7af84325cd
Ritikraj551 <rtkrajs551@gmail.com>
2025-12-01 04:20:21 +0530

    Finalized dashboard functionality

--- Frontend/src/pages/educator/Dashboard.jsx
+++ Frontend/src/pages/educator/Dashboard.jsx
@@ -2,13 +2,45 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { FaArrowLeftLong } from "react-icons/fa6";
+import {
+  ResponsiveContainer,
+  Bar,
+  BarChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+} from "reacharts";
 
 const Dashboard = () => {
   const { userData } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const { creatorCourseData } = useSelector((state) => state.course);
+
+  const courseProgressData =
+    creatorCourseData?.map((course) => ({
+      name: course.title?.slice(0, 10) + "...",
+      lectures: course.lectures?.length || 0,
+    })) || [];
+
+  const enrollData =
+    creatorCourseData?.map((course) => ({
+      name: course.title?.slice(0, 10) + "...",
+      enrolled: course.enrollStudents?.length || 0,
+    })) || [];
+
+  const totalEarning = creatorCourseData?.reduce((sum,course)=>{
+    const studentCount = course.enrolledStudents?.length||0;
+    const courseRevenue = course.price?course.price*studentCount:0
+    return sum+courseRevenue
+  },0)||0
+
   return (
     <div className="flex min-h-screen bg-gray-100">
-      <FaArrowLeftLong onClick={()=>navigate("/")} className="w-[22px] absolute top-[10%] left-[10%] h-[22px] cursor-pointer"/>
+      <FaArrowLeftLong
+        onClick={() => navigate("/")}
+        className="w-[22px] absolute top-[10%] left-[10%] h-[22px] cursor-pointer"
+      />
       <div className="w-full px-6 py-10 bg-gray-50 space-y-10">
         {/* Main section */}
         <div className="max-w-5xl mx-auto bg-white rounded-xl shadow-md p-6 flex flex-col md:flex-row items-center gap-6">
@@ -22,7 +54,7 @@ const Dashboard = () => {
               Welcome,{userData?.name || "Educator"}
             </h1>
             <h1 className="text-xl font-semibold text-gray-800">
-              Total Earning : 0
+              Total Earning : ₹{totalEarning.toLocaleString}
             </h1>
             <p className="text-gray-600 text-sm">
               {userData?.description ||
@@ -38,7 +70,39 @@ const Dashboard = () => {
         </div>
 
         {/* Graph section */}
-        <div></div>
+        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+          {/* for course progress graph */}
+          <div className="bg-white rounded-lg shadow-md p-6">
+            <h2 className="text-lg font-semibold mb-4">
+              Course Progress (Lectures)
+            </h2>
+            <ResponsiveContainer width="100%" height={300}>
+              <BarChart data={courseProgressData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="lectures" fill="black" radius={[5, 5, 0, 0]} />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+
+          {/* enrolled data */}
+          <div className="bg-white rounded-lg shadow-md p-6">
+            <h2 className="text-lg font-semibold mb-4">
+              Students Enrollment
+            </h2>
+            <ResponsiveContainer width="100%" height={300}>
+              <BarChart data={enrollData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="enrolled" fill="black" radius={[5, 5, 0, 0]} />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
       </div>
     </div>
   );
